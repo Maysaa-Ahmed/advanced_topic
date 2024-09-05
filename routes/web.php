1b27e83021421686b4f2094b4abf08f98712c1ce
maysaa <maysaa.ahmed94@gmail.com>
2024-09-04 18:21:19 -0700

    users

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// Admin => Users
+Route::get('users', [UserController::class, 'index'])->name('users.index');
+
+
+//Admin => Topics
+
+
+
+//Admin => Categories
+
+
+
+//Admin => Testimonials
+
+
+
+//Admin => Messages
